40890d8a3bd7376c6b4bbde00813efee286364fa
Kienaan Abdi <abdifiiq@google.com>
2022-08-19 14:43:52 -0500

    first commit of the project

---
+++ src/api/post/routes/post.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * post router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::post.post', {
+    config: {
+        find: {
+            policies: ['is-admin']
+        }
+    }
+});
